53c327b56ee2187d5932797d68a2d41a37f8fdc4
Jake Webster <jakepeterwebster@gmail.com>
2020-12-06 14:28:36 +1000

    submit button puts into into a list

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
 console.log("App starts");
 const todoList = [];
+$("#container").append('<ul id="todos"></ul>');
 const onTodoFormSubmit = (event) => {
   event.preventDefault();
   const $todo = $('input[name="todo"]');
   const todoItem = { title: $todo.val(), done: false };
   todoList.push(todoItem);
-  console.log("todoListArray=", todoList);
+  console.log("todoList", todoList);
+  // The best approach
+  $("#todos").append(`<li>${todoList[todoList.length - 1].title}</li>`);
+  // Other approaches
+  // $("#todos").append(`<li>${$todo.val()}</li>`);
+  // $("#todos").empty();
+  // for (let i = 0; i < todoList.length; i++) {
+  //   // todoList[i].title
+  //   $("#todos").append(`<li>${todoList[i].title}</li>`);
+  // }
 };
 $("#todo-form").on("submit", onTodoFormSubmit);
